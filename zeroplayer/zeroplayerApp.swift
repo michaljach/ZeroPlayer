b05601d3062918818747a8f15cb7d115dfd77253
Michal Jach <michaljach@gmail.com>
2026-02-13 22:38:56 +0100

    Initial Commit

---
+++ zeroplayer/zeroplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  zeroplayerApp.swift
+//  zeroplayer
+//
+//  Created by Michal Jach on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct zeroplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
